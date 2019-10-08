9382ac1d9bc11b8a37c83737f985c8ee49e2bf2f
Gabriel Olescki <glescki@gmail.com>
2019-10-08 13:18:27 -0300

    Add tmux installation and tmux configuration file

--- install.sh
+++ install.sh
@@ -2,24 +2,24 @@
 
 # ## GIT
 # # Make global git settings
-# git config --global core.editor "vim"
-# git config --global user.email "glecki@gmail.com"
-# git config --global user.name "Gabriel Olescki"
+git config --global core.editor "vim"
+git config --global user.email "glecki@gmail.com"
+git config --global user.name "Gabriel Olescki"
 
 # ## ZSH
 #
 # # Install zsh
-# apt-get install zsh
+apt-get install zsh
 #
 # # Make it the default shell
 #
-# chsh -s /usr/bin/zsh
+chsh -s /usr/bin/zsh
 #
 # # Install oh-my-zsh
-# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #
 # # Install powerlevel10k
-# git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 ## VIM
 
@@ -33,6 +33,10 @@ cp ./vim/.vimrc ~
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 # Install all plugins in .vimrc file
 vim +PluginInstall +qall
 
@@ -41,3 +45,15 @@ vim +PluginInstall +qall
 
 # Update all plugins (kinda redundant)
 vim +PluginUpdate +qall
+
+## TMUX
+
+# Install tmux plugin manager 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Copy ./tmux/.tmux.conf to ~/.tmux.conf
+# First create a backup ~/.tmuxrc called .tmux.conf.old
+cp ~/.tmux ~/.tmux.conf.old
+
+# Then overwrites ~/.tmuxrc with local .tmux.conf file
+cp ./tmux/.tmux.conf ~
